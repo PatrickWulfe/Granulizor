e0cb5782985e7b18f159b3afdd2d35e9393b41d2
Patrick Wulfe <p_wulfe@yahoo.com>
2018-07-31 17:38:46 -0700

    Getting started, adding file selection for audio source and decoding to get sample information from source.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,49 @@
 #[macro_use]
 extern crate vst;
 extern crate hound;
+extern crate nfd;
 
 use vst::plugin::{Info, Plugin, Category};
 use vst::buffer::AudioBuffer;
-use vst::api::FileSelect;
 use hound;
+use nfd::Response;
 
 #[derive(Default)]
-struct Granulizer;
+struct Granulizor {
+    grain_size: f32,
+    source_samples: Vec<f32>,
+}
 
-impl Plugin for Granulizer {
+impl Default for Granulizor {
+    fn default() -> Granulizor {
+        let mut sample_select = String::new();
+        while sample_select.is_empty() {
+            let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
+                panic!(e);
+            });
+            match result {
+                Response::Okay(file_path) => sample_select = file_path,
+                Response::OkayMultiple(files) => println!("You Must Select a Single File"),
+                Response::Cancel => println!("Please Select a Sample"),
+            }
+        }
+        let mut reader = hound::WavReader::open(sample_select).unwrap();
+        Granulizor {
+            grain_size: 20_f32,
+            source_samples: reader.samples::<i32>();
+            }),
+        }
+    }
+}
+
+impl Plugin for Granulizor {
     fn get_info(&self) -> Info {
         Info {
             name: "Granulizor".to_string(),
             unique_id: 3332,
             inputs: 0,
             outputs: 2,
+            parameters: 1,
             category: Category::Synth,
             ..Default::default()
         }
